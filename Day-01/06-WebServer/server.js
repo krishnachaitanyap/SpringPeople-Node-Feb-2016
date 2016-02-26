aa68de6595de3e8131885c5736914a572fc5d912
Magesh <tkmagesh77@gmail.com>
2016-02-26 15:00:30 -0800

    static web server - assignment

--- Day-01/06-WebServer/server.js
+++ Day-01/06-WebServer/server.js
@@ -1,7 +1,35 @@
 var http = require('http');
-var server = http.createServer(function(req, res){
-	res.write('<h1>Welcome to node.js</h1>');
+/*
+req.url -> resource requested
+fs.existsSync -> if a file exists or not
+__dirname -> current folder
+path.join(__dirname, req.url) -> actual physical path of the resource
+if (file not exists){
+	res.statusCode = 404;
 	res.end();
+}
+}
+*/
+function getFileContents(callback){
+	var fs = require('fs');
+	fs.readFile('./index.html', {encoding : 'utf8'}, function(err, fileContents){
+		if (err){
+			console.log('something went wrong - ', err);
+			process.exit(1);
+		}
+		callback(fileContents);
+	});
+}
+function getResponseWriter(req, res){
+	return function writeResponse(fileContents){
+		res.write(fileContents);
+		res.end();
+	}	
+}
+
+var server = http.createServer(function(req, res){
+	console.log('req for - > ', req.url);
+	getFileContents(getResponseWriter(req, res));
 });
 server.listen(8080);
 console.log('server listening on port 8080..!!');
