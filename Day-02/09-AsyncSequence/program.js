bf7e585a5d1c545c2cdb2aed9ed66e980955eb37
Magesh <tkmagesh77@gmail.com>
2016-02-27 10:51:35 -0800

    Async Sequence - Solution

--- Day-02/09-AsyncSequence/program.js
+++ Day-02/09-AsyncSequence/program.js
@@ -16,44 +16,59 @@ function f4Sync(){
 	console.log('f4Sync Completed');
 }
 
-module.exports.runSync = function(){
-	f1Sync();
-	f2Sync();
-	f3Sync();
-	f4Sync();
-}
+var syncFns = [f1Sync, f2Sync, f3Sync, f4Sync];
 
-function f1(){
+module.exports.runSync = function(){
+	for(var i=0; i<syncFns.length; i++)
+		syncFns[i]();
+};
+
+
+function f1(next){
 	console.log("f1 Started");
 	setTimeout(function(){
 		console.log("f1 Completed");
+		if (next) next();
 	},3000);
 }
 
-function f2(){
+function f2(next){
 	console.log("f2 Started");
 	setTimeout(function(){
 		console.log("f2 Completed");
+		if (next) next();
 	},3000);
 }
 
-function f3(){
+function f3(next){
 	console.log("f3 Started");
 	setTimeout(function(){
 		console.log("f3 Completed");
+		if (next) next();
 	},3000);
 }
 
-function f4(){
+function f4(next){
 	console.log("f4 Started");
 	setTimeout(function(){
 		console.log("f4 Completed");
+		if (next) next();
 	},3000);
 }
 
+var fns = [f1, f2, f3, f4];
+
 module.exports.run =function(){
-	f1();
-	f2();
-	f3();
-	f4();
+	function exec(fns){
+		var first = fns[0],
+			remaining = fns.slice(1),
+			next = function(){
+				exec(remaining);
+			};
+			if (first){
+				first(next);
+			}
+	}
+	exec(fns);
 };
+
